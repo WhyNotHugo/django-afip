7872ce73180386996be962b1f42459959365e79d
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-09-07 12:34:45 -0300

    Add hack to install proper version of suds
    Add a nasty hack that installs the proper version of suds either is using `pip
    install -r' or `setup.py'.
    
    Upstream HEAD is broken, and not stable release work properly. The intention is
    to fork suds at some point and keep that alive, but for now, this'll work.

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
     license='ISC',
     packages=find_packages(),
     # long_description=open('README.rst').read(),
-    install_requires=open('requirements.txt').read().splitlines()[:-1],
+    install_requires=open('requirements.txt').read().splitlines()[:-1] +
+        ['suds-py3==1.0.0.0'],
     dependency_links=(
         'git+https://github.com/hobarrera/suds-py3.git#egg=suds-py3-1.0.0.0',
     ),
