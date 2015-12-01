e6de7a2b5fa33caec90497c23c38765d0874004d
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-12-01 15:21:29 -0300

    Reduce number of queries performed by ReceiptAdmin

--- django_afip/admin.py
+++ django_afip/admin.py
@@ -119,8 +119,12 @@ class ReceiptAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         return super().get_queryset(request) \
-            .select_related('validation') \
-            .select_related('receipt_type')
+            .select_related(
+                'validation',
+                'receipt_type',
+                'point_of_sales',
+                'batch',
+            )
 
     def validated(self, obj):
         return obj.validation.result == models.Validation.RESULT_APPROVED
