bda922322a1efca8b7c04a2a451139d41d818e79
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-03-14 23:34:13 -0300

    Tidy up test files
    Move everything into a `tests` package, and centralize all
    mock-factories in the same place.

--- testapp/testapp/testmain/mocks.py
+++ testapp/testapp/testmain/mocks.py
@@ -1,13 +1,14 @@
+from datetime import date
 from tempfile import NamedTemporaryFile
 
 from django.contrib.auth.models import User
 from django.core.files.base import File
 
-from django_afip.models import TaxPayer
+from django_afip import models
 
 
 def taxpayer(key=None):
-    taxpayer = TaxPayer.objects.create(
+    taxpayer = models.TaxPayer.objects.create(
         name='Test Taxpayer',
         cuit=20329642330,
         is_sandboxed=True,
@@ -24,3 +25,55 @@ def taxpayer(key=None):
 
 def superuser():
     return User.objects.create_superuser('test', 'test@example.com', '123')
+
+
+def receipt(
+        document_type=96,
+        with_tax=True,
+        with_vat=True,
+        receipt_type=6,
+     ):
+    """
+    Return a dummy mocked-receipt.
+
+    This function is here for convenience only and has no special magic other
+    than creating a receipt with valid Vat and Tax attributes.
+    """
+    total_amount = 100
+    if with_tax:
+        total_amount += 9
+    if with_vat:
+        total_amount += 21
+    receipt = models.Receipt.objects.create(
+        concept=models.ConceptType.objects.get(code=1),
+        document_type=models.DocumentType.objects.get(
+            code=document_type,
+        ),
+        document_number='203012345',
+        issued_date=date.today(),
+        total_amount=total_amount,
+        net_untaxed=0,
+        net_taxed=100,
+        exempt_amount=0,
+        currency=models.CurrencyType.objects.get(code='PES'),
+        currency_quote=1,
+
+        receipt_type=models.ReceiptType.objects.get(code=receipt_type),
+        point_of_sales=models.PointOfSales.objects.first(),
+    )
+    if with_vat:
+        models.Vat.objects.create(
+            vat_type=models.VatType.objects.get(code=5),
+            base_amount=100,
+            amount=21,
+            receipt=receipt,
+        )
+    if with_tax:
+        models.Tax.objects.create(
+            tax_type=models.TaxType.objects.get(code=3),
+            base_amount=100,
+            aliquot=9,
+            amount=9,
+            receipt=receipt,
+        )
+    return receipt
