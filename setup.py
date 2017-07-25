3154a8bd09e8d445de6893862c646c9fb983021d
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-07-25 20:25:09 -0300

    Add classifiers for py35 and py36

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
