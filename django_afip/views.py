de0bbf978695d206189ee4effb124234968525cb
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-07-30 14:15:59 -0300

    Add a view to display PDF receipts
    Fixes #23
    Closes !7
    Closes !8

--- django_afip/views.py
+++ django_afip/views.py
@@ -6,7 +6,7 @@ from .pdf import generate_receipt_pdf
 
 
 class ReceiptHTMLView(View):
-
+    """Renders a receipt as HTML."""
     def get(self, request, pk):
         return HttpResponse(
             generate_receipt_pdf(pk, request, True),
@@ -14,11 +14,24 @@ class ReceiptHTMLView(View):
 
 
 class ReceiptPDFView(View):
+    """Renders a receipt as a PDF, prompting to download it."""
 
     def get(self, request, pk):
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename=' + \
             _('receipt %s.pdf' % pk)
 
+        generate_receipt_pdf(pk, response)
+        return response
+
+
+class ReceiptPDFDisplayView(View):
+    """
+    Renders a receipt as a PDF.
+
+    Browsers should render the file, rather than prompt to download it.
+    """
+    def get(self, request, pk):
+        response = HttpResponse(content_type='application/pdf')
         generate_receipt_pdf(pk, response)
         return response
