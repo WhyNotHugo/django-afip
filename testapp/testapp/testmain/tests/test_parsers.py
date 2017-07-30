7ebd68fab54ae697bd3da04704102ed77b387460
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-07-30 15:50:09 -0300

    Add tests for parse_date and parse_datetime

---
+++ testapp/testapp/testmain/tests/test_parsers.py
@@ -0,0 +1,35 @@
+from datetime import date, datetime
+
+import pytz
+from django.test import TestCase
+
+from django_afip import parsers
+
+
+class ParseDatetimeTestCase(TestCase):
+    def test_parse_null(self):
+        self.assertEqual(parsers.parse_datetime('NULL'), None)
+
+    def test_parse_none(self):
+        self.assertEqual(parsers.parse_datetime(None), None)
+
+    def test_parse_datetimes(self):
+        tz = pytz.timezone(pytz.country_timezones['ar'][0])
+        self.assertEqual(
+            parsers.parse_datetime('20170730154330'),
+            datetime(2017, 7, 30, 15, 43, 30, tzinfo=tz),
+        )
+
+
+class ParseDateTestCase(TestCase):
+    def test_parse_null(self):
+        self.assertEqual(parsers.parse_date('NULL'), None)
+
+    def test_parse_none(self):
+        self.assertEqual(parsers.parse_date(None), None)
+
+    def test_parse_dates(self):
+        self.assertEqual(
+            parsers.parse_date('20170730'),
+            date(2017, 7, 30),
+        )
