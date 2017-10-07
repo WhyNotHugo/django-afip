0e3ad23a313f2e99f63caebb2eb3beb470f30b5f
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-10-07 19:27:14 -0300

    Fix DB not being retained by the test app

--- testapp/testapp/settings.py
+++ testapp/testapp/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'testapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite://db.sqlite3'),
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3'),
 }
 
 
