58efea7b18dab5b67d36a06f3f7964d8c1d3a772
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-03-02 19:50:25 +0100

    Avoid primary key issues in future Djangos

--- django_afip/apps.py
+++ django_afip/apps.py
@@ -5,6 +5,7 @@ class AfipConfig(AppConfig):
     name = "django_afip"
     label = "afip"
     verbose_name = "AFIP"
+    default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
         # Register app signals:
