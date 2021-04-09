597caaa465712af706ac9e09d37757ad682a8f8d
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-04-09 18:54:01 +0200

    Add a check that there are no pending migrations

---
+++ testapp/testapp/testmain/tests/test_migrations.py
@@ -0,0 +1,10 @@
+import pytest
+from django.core.management import call_command
+
+
+@pytest.mark.django_db
+def test_no_missing_migrations():
+    """Check that there are no missing migrations of the app."""
+
+    # This returns pending migrations -- or false if non are pending.
+    assert not call_command("makemigrations", check=True)
