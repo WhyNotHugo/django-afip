7b078b295ffd43262834f9b12e1497b340a9d38c
Hugo Osvaldo Barrera <hugo@barrera.io>
2022-05-26 17:28:08 +0200

    These tests also use network

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -5,6 +5,7 @@ import pytest
 from django_afip.clients import get_client
 
 
+@pytest.mark.live
 def test_services_are_cached():
     service1 = get_client("wsfe", False)
     with patch.dict("django_afip.clients.WSDLS", values={}, clear=True):
@@ -13,6 +14,7 @@ def test_services_are_cached():
     assert service1 is service2
 
 
+@pytest.mark.live
 def test_inexisting_service():
     with pytest.raises(ValueError, match="Unknown service name, nonexistant"):
         get_client("nonexistant", False)
