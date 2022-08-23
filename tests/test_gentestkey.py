139c7d474022ef6db00b6261b878928f8ce4de7a
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2022-08-23 19:50:14 +0200

    Add helper to renew test certificate

---
+++ tests/test_gentestkey.py
@@ -0,0 +1,30 @@
+import os
+
+import pytest
+
+from django_afip import factories
+
+
+@pytest.mark.skipif(os.environ.get("GENTESTCSR") is None, reason="not a test")
+@pytest.mark.django_db
+def test_generate_test_csr():
+    """Generate a new test CSR (this is not really a test)
+
+    Run this with:
+
+        GENTESTCSR=yes tox -e py-sqlite -- -k test_generate_test_csr
+    """
+
+    # This one is used for most tests.
+    taxpayer = factories.TaxPayerFactory(is_sandboxed=True)
+
+    csr = taxpayer.generate_csr("wsfe")
+    with open("test.csr", "wb") as f:
+        f.write(csr.read())
+
+    # This one is used for the `test_authentication_with_bad` test.
+    taxpayer = factories.AlternateTaxpayerFactory(is_sandboxed=True)
+
+    csr = taxpayer.generate_csr("wsfe")
+    with open("test2.csr", "wb") as f:
+        f.write(csr.read())
