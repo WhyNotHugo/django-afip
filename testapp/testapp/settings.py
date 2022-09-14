d0cf1c086f1098b34d245552e64c565c61623091
erebodino <erebodino@gmail.com>
2022-09-14 17:01:12 -0300

    Modified Receipt and validation model to register CAEA validation, modified _validate to process creation of ReceiptValidation

--- testapp/testapp/settings.py
+++ testapp/testapp/settings.py
@@ -63,13 +63,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "testapp.wsgi.application"
 
 # Database
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-DATABASES = {"default": dj_database_url.config()}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+#DATABASES = {"default": dj_database_url.config()}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
