ce4a151d5ad9b2dca90d6b9b5f986b019b6a5339
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-04-03 15:29:27 +0200

    Mark django_renderpdf~4.0.0 as compatible

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     install_requires=[
         "cryptography>=3.2,<40",
         "django>=3.2,<4.2",
-        "django_renderpdf>=3.0.0,<4.0.0",
+        "django_renderpdf>=3.0.0,<5.0.0",
         "lxml>=3.4.4",
         "pyopenssl>=16.2.0",
         'backports.zoneinfo;python_version<"3.9"',
