14ec0fbd06b91f0e6b446b085f29a13b4dc2b44e
alvezgr <alvezpope@gmail.com>
2023-04-12 17:51:29 -0300

    Added migrations that creates Optionals models and extends max_length of generic type code to four.

---
+++ django_afip/migrations/0012_optionaltype_optional_alter_code_in_generics.py
@@ -0,0 +1,117 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("afip", "0011_receiptentry_discount_and_more"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="OptionalType",
+            fields=[
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("code", models.CharField(max_length=3, verbose_name="code")),
+                (
+                    "description",
+                    models.CharField(max_length=250, verbose_name="description"),
+                ),
+                (
+                    "valid_from",
+                    models.DateField(blank=True, null=True, verbose_name="valid from"),
+                ),
+                (
+                    "valid_to",
+                    models.DateField(blank=True, null=True, verbose_name="valid until"),
+                ),
+            ],
+            options={
+                "verbose_name": "optional type",
+                "verbose_name_plural": "optional types",
+            },
+        ),
+        migrations.CreateModel(
+            name="Optional",
+            fields=[
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "value",
+                    models.CharField(max_length=250, verbose_name="optional value"),
+                ),
+                (
+                    "optional_type",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to="afip.optionaltype",
+                        verbose_name="optional type",
+                    ),
+                ),
+                (
+                    "receipt",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        related_name="optionals",
+                        to="afip.receipt",
+                    ),
+                ),
+            ],
+            options={
+                "verbose_name": "optional",
+                "verbose_name_plural": "optionals",
+            },
+        ),
+        # alter code operations
+        migrations.AlterField(
+            model_name="concepttype",
+            name="code",
+            field=models.CharField(max_length=4, verbose_name="code"),
+        ),
+        migrations.AlterField(
+            model_name="currencytype",
+            name="code",
+            field=models.CharField(max_length=4, verbose_name="code"),
+        ),
+        migrations.AlterField(
+            model_name="documenttype",
+            name="code",
+            field=models.CharField(max_length=4, verbose_name="code"),
+        ),
+        migrations.AlterField(
+            model_name="optionaltype",
+            name="code",
+            field=models.CharField(max_length=4, verbose_name="code"),
+        ),
+        migrations.AlterField(
+            model_name="receipttype",
+            name="code",
+            field=models.CharField(max_length=4, verbose_name="code"),
+        ),
+        migrations.AlterField(
+            model_name="taxtype",
+            name="code",
+            field=models.CharField(max_length=4, verbose_name="code"),
+        ),
+        migrations.AlterField(
+            model_name="vattype",
+            name="code",
+            field=models.CharField(max_length=4, verbose_name="code"),
+        ),
+    ]
