cb0f6c3a369c9777d061dd4ef3a1e3647170a1f8
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-05-08 13:42:15 +0200

    Add test to confirm that insecure hack is required

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -1,6 +1,8 @@
 from unittest.mock import patch
 
 import pytest
+import requests
+from requests.exceptions import SSLError
 
 from django_afip.clients import get_client
 
@@ -16,3 +18,9 @@ def test_services_are_cached():
 def test_inexisting_service():
     with pytest.raises(ValueError, match="Unknown service name, nonexistant"):
         get_client("nonexistant", False)
+
+
+@pytest.mark.live()
+def test_insecure_dh_hack_required():
+    with pytest.raises(SSLError, match="SSL: DH_KEY_TOO_SMALL. dh key too small"):
+        requests.get("https://servicios1.afip.gov.ar/wsfev1/service.asmx?WSDL")
