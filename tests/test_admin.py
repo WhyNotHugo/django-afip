932ea2ac9149625623dcf2d09cf55b8302c8e912
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-05-08 12:50:40 +0200

    Restrict more typing exceptions

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -14,8 +14,8 @@ from pytest_django.asserts import assertNotContains
 from django_afip import exceptions
 from django_afip import factories
 from django_afip import models
-from django_afip.admin import ReceiptAdmin  # type: ignore
-from django_afip.admin import catch_errors  # type: ignore
+from django_afip.admin import ReceiptAdmin  # type: ignore[attr-defined]
+from django_afip.admin import catch_errors  # type: ignore[attr-defined]
 
 
 def test_certificate_expired():
