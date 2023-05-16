8ffbea3ee53b648f40106ea90404e276118c57e5
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-05-16 16:41:12 +0200

    Remove no-op 'pass' statements

--- django_afip/exceptions.py
+++ django_afip/exceptions.py
@@ -26,15 +26,11 @@ class AuthenticationError(DjangoAfipException):
     Raised when there is a non-specific error during an authentication attempt.
     """
 
-    pass
-
 
 class CertificateExpired(AuthenticationError):
     """
     Raised when an authentication was attempted with an expired certificate.
     """
-
-    pass
 
 
 class UntrustedCertificate(AuthenticationError):
@@ -42,16 +38,12 @@ class UntrustedCertificate(AuthenticationError):
     Raise when an untrusted certificate is used in an authentication attempt.
     """
 
-    pass
-
 
 class CorruptCertificate(AuthenticationError):
     """
     Raised when a corrupt ceritificate file is used in an authenticaiton
     attempt.
     """
-
-    pass
 
 
 class CannotValidateTogether(DjangoAfipException):
