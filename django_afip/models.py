5492f5a7c4f657803d2e55863c84a97c4e780384
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-09-24 22:09:49 +0200

    docs: add hint for ReceiptManager

--- django_afip/models.py
+++ django_afip/models.py
@@ -1201,6 +1201,9 @@ class Receipt(models.Model):
         blank=True,
     )
 
+    #: The default manager includes extra methods including helpers for validation.
+    #:
+    #: See :class:`~.ReceiptManager`.
     objects = ReceiptManager()
 
     # TODO: methods to validate totals
