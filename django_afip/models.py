7599087252e7c05468669292b4ea7962dc06b01b
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2026-08-17 13:22:48 +0200

    Drop support for Python < 3.13
    Even 3.13 is near its EOL, so drop support for older, unmaintained
    versions.

--- django_afip/models.py
+++ django_afip/models.py
@@ -7,17 +7,15 @@ import random
 import re
 import warnings
 from contextlib import suppress
+from datetime import UTC
 from datetime import datetime
 from datetime import timedelta
-from datetime import timezone
 from decimal import Decimal
 from io import BytesIO
 from typing import TYPE_CHECKING
 from typing import BinaryIO
 from typing import ClassVar
-from typing import Generic
 from typing import Literal
-from typing import TypeVar
 from uuid import uuid4
 
 from django.conf import settings
@@ -157,10 +155,7 @@ def _get_storage_from_settings(
     return default_storage
 
 
-_T = TypeVar("_T", bound="GenericAfipType", covariant=True)
-
-
-class GenericAfipTypeManager(models.Manager, Generic[_T]):
+class GenericAfipTypeManager[T: "GenericAfipType"](models.Manager):
     """Default Manager for GenericAfipType."""
 
     def __init__(self, service_name: str, type_name: str) -> None:
@@ -194,7 +189,7 @@ class GenericAfipTypeManager(models.Manager, Generic[_T]):
                 valid_to=parsers.parse_date_maybe(result.FchHasta),
             )
 
-    def get_by_natural_key(self, code: str) -> _T:
+    def get_by_natural_key(self, code: str) -> T:
         return self.get(code=code)
 
     def exists_by_natural_key(self, code: str) -> bool:
@@ -508,7 +503,7 @@ class TaxPayer(models.Model):
             return None
         datestring = not_after.decode()
         dt = datetime.strptime(datestring, "%Y%m%d%H%M%SZ")
-        return dt.replace(tzinfo=timezone.utc)
+        return dt.replace(tzinfo=UTC)
 
     def generate_key(self, force: bool = False) -> bool:
         """Creates a key file for this TaxPayer
@@ -572,7 +567,7 @@ class TaxPayer(models.Model):
         instead.
         """
         return self.auth_tickets.filter(
-            expires__gt=datetime.now(timezone.utc),
+            expires__gt=datetime.now(UTC),
             service=service,
         ).last()
 
@@ -767,7 +762,7 @@ class AuthTicketManager(models.Manager["AuthTicket"]):
         """Return a valid, active ticket for a given service."""
         ticket = AuthTicket.objects.filter(
             token__isnull=False,
-            expires__gt=datetime.now(timezone.utc),
+            expires__gt=datetime.now(UTC),
             service=service,
         ).first()
         if ticket:
--- docs/changelog.rst
+++ docs/changelog.rst
@@ -13,6 +13,11 @@ Check the `latest tags`_ or `PyPI`_ for the latest stable release.
 
 Any breaking changes which require intervention will be mentioned here.
 
+14.2.0
+------
+
+- Drop support for Python < 3.13.
+
 14.1.0
 ------
 
